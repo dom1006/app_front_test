5aeadf75384db1df9e79664d8e2652e596c6f90e
Dominik Ciechowski <dominik.ciechowski@e-qsg.com>
2018-01-10 11:04:48 +0100

    commit

---
+++ src/test/java/LogInTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+
+public class LogInTest {
+    static WebDriver driver;
+
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    @Test(priority = 1)
+    public void logIn() throws InterruptedException {
+        driver.get("https://192.168.1.77/login.html");
+        Thread.sleep(5000);
+        driver.close();
+    }
+}
